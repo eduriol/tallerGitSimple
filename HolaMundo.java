f68c5eb1ba70d414a4b770e778a1c0a354c5e82c
Eduardo Riol Fenández <eduriol@gmail.com>
2017-02-03 12:52:27 +0100

    se extiende el saludo

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,6 +3,7 @@ public class HolaMundo {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hola Mundo");
+        System.out.println("Qué tal?");
     }
 
 }
